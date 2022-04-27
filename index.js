70d9f0e644476a00843e01c5b4cd92632d928bcf
Semaj Holmes <semaj27holmes@gmail.com>
2022-04-27 00:18:36 -0400

    added all of the const's

---
+++ index.js
@@ -0,0 +1,6 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const tiles = Array.from(document.querySelectorAll('.tile'));
+    const playerDisplay = document.querySelector('.display-player');
+    const resetButton = document.querySelector('#reset');
+    const announcer = document.querySelector('.announcer');
+    
